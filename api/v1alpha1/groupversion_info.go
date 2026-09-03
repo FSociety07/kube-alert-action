412ad2f8ac1af0cf5524038dc591b96f2792dbf6
sawn <fakerendu@gmail.com>
2026-09-03 17:22:46 +0530

    group marker

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,16 +1,20 @@
+// Package v1alpha1 contains API Schema definitions for the sawnt.xyz v1alpha1 API group
+// +kubebuilder:object:generate=true
+// +groupName=sawnt.xyz
+
 package v1alpha1
 
 import (
-    "k8s.io/apimachinery/pkg/runtime/schema"
-    "sigs.k8s.io/controller-runtime/pkg/scheme"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
-    GroupVersion = schema.GroupVersion{Group: "sawnt.xyz", Version: "v1alpha1"}
-    SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
-    AddToScheme = SchemeBuilder.AddToScheme
+	GroupVersion  = schema.GroupVersion{Group: "sawnt.xyz", Version: "v1alpha1"}
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	AddToScheme   = SchemeBuilder.AddToScheme
 )
 
 func init() {
-    SchemeBuilder.Register(&ActionMap{}, &ActionMapList{}, &AlertEvent{}, &AlertEventList{})
+	SchemeBuilder.Register(&ActionMap{}, &ActionMapList{}, &AlertEvent{}, &AlertEventList{})
 }
