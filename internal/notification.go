67a562d36b75fa935cc63e2f45f50675216562c5
sawn <fakerendu@gmail.com>
2026-09-20 20:36:25 +0530

    gchat notification

---
+++ internal/notification.go
@@ -0,0 +1,70 @@
+package notify
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+type ChatMessage struct {
+	Text string `json:"text"`
+}
+
+type Notifier struct {
+	WebhookURL string
+}
+
+func (n *Notifier) SendMessage(ctx context.Context, msg string, threadKey string) error {
+	log := logf.FromContext(ctx)
+
+	//build msg format and URL
+	payload := ChatMessage{Text: msg}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		log.Error(err, "Error marshalling payload")
+		return err
+	}
+
+	u, err := url.Parse(n.WebhookURL)
+	if err != nil {
+		log.Error(err, "Error parsing url", "WebhookURL", n.WebhookURL)
+		return err
+	}
+
+	q := u.Query()
+
+	q.Set("threadKey", threadKey)
+	u.RawQuery = q.Encode()
+	finalURL := u.String()
+
+	//Send HTTP call
+	req, err := http.NewRequestWithContext(ctx, "POST", finalURL, bytes.NewReader(body))
+	if err != nil {
+		log.Error(err, "Error creating HTTP Request")
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Error(err, "Error sending HTTP Request")
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	respBody, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		log.Error(nil, "Chat webhook returned non-OK status", "status", resp.StatusCode, "response", respBody)
+		return fmt.Errorf("chat webhook returned status %d with response %s", resp.StatusCode, respBody)
+	}
+
+	return nil
+}
